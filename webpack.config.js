3db82d2a498cea60624737787a00f27168b24adc
Marcel Lindig <ml@nullpilot.de>
2020-01-14 00:54:06 +0100

    add basic build process

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const glob = require('glob');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = (env, options) => ({
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: false,
+        test: /\.min\.js(\?.*)?$/i,
+      })
+    ]
+  },
+  entry: {
+    'poisson': './src/poisson-disk-sampling.js',
+    'poisson.min': './src/poisson-disk-sampling.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+});
